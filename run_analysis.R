9b3a92be71b7e305ae2eea54d01fa567cb03f67e
Ed Murphy <emurphy@babycenter.com>
2015-11-22 11:23:53 -0800

    Added comments, changed output to .txt files
    - added comments that directly correspond to the 5 steps in the
    assignment.
    - renamed output files to .txt files (they were .dt files)

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,32 @@
 library(data.table)
 library(reshape2)
-#if(!dir.exists("./download")) {dir.create("./download")}
-#download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./download/dataset.zip")
-#unzip("./download/dataset.zip")
 
-# load the set of test and train data (2947 rows, 561 columns)
+if(!dir.exists("./download")) {dir.create("./download")}
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./download/dataset.zip")
+unzip("./download/dataset.zip")
+
+##
+## 1. Merges the training and the test sets to create one data set.
+##
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 dataSet <- rbind(X_test, X_train)
 
-# apply the column names
+# apply the column names in the features file
 features <- readLines("UCI HAR Dataset/features.txt")
 colnames(dataSet) <- features
 
-# select only the mean and standard deviation columns
+##
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+##
 mean_features <- grep("-mean()", features, fixed=TRUE, value=TRUE)
 std_features <- grep("-std()", features, fixed=TRUE, value=TRUE)
 mean_std_features <- c(mean_features, std_features)
 dataSet <- dataSet[,mean_std_features]
 
+##
+## 4. Appropriately labels the data set with descriptive variable names.
+##
 # rename the columns;
 # - remove initial digits
 # - initial t is time, intial f is freq (frequency)
@@ -50,7 +58,9 @@ activity_train <- readLines("UCI HAR Dataset/train/y_train.txt")
 activity <- c(activity_test, activity_train)
 activity <- as.numeric(activity) # convert to numeric vector
 
-# convert activity numbers to a human readable label (based on UCI HAR Dataset/activity_labels.txt)
+##
+## 3. Uses descriptive activity names to name the activities in the data set
+##
 activity_labels = c("walking", "walkingUpStairs", "walkingDownStairs", "sitting", "standing", "laying") # readLines("UCI HAR Dataset/activity_labels.txt")
 activity <- factor(activity, labels=activity_labels)
 
@@ -58,14 +68,15 @@ activity <- factor(activity, labels=activity_labels)
 dataSet$activity <- activity
 
 # save this dataset
-write.table(dataSet, "SubjectActivityFeature.dt")
+write.table(dataSet, "SubjectActivityFeature.txt")
 
-#
-# create a table with the average value for each feature for each subject/activity pair
-#
+##
+## 5. From the data set in step 4, creates a second, independent tidy data set 
+##    with the average of each variable for each activity and each subject.
+##
 meltData <- melt(dataSet, c("subject", "activity"))
 meanData <- dcast(meltData, subject+activity ~ variable, fun=mean)
 
 # write out the averages data set
-write.table(meanData, "SubjectActivityFeatureMean.dt")
+write.table(meanData, "SubjectActivityFeatureMean.txt", row.name=FALSE)
 
